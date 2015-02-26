90e6a9f02004f06ffa3ccc7b73b9462fc379bc3f
Jigish Patel <jigish.patel@gmail.com>
2015-02-26 10:11:44 -0800

    adding uncommitted code

--- http-inbound-xml/src/main/java/com/senyume/integration/service/InboundService.java
+++ http-inbound-xml/src/main/java/com/senyume/integration/service/InboundService.java
@@ -1,5 +1,8 @@
 package com.senyume.integration.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.Map;
 
 /**
@@ -8,9 +11,14 @@ import java.util.Map;
 
 public class InboundService {
 	
-	public String handleIncomingRequest(Map<String,Object> headers, String payload){
+	private static Logger logger = LoggerFactory.getLogger(InboundService.class);
+	private int counter = 0;
+	
+	public String handleIncomingRequest(Map<String,Object> headers, String payload) throws Exception{
 		String restServiceUrl = (String) headers.get("http_requestUrl");
 		String restRequestMethod = (String) headers.get("http_requestMethod");
+		logger.info("counter=" + ++counter);
+		Thread.sleep(5000);
 		return "REST API method: " + restServiceUrl
 				+ "\nrequest verb: " + restRequestMethod
 				+ "\nrequest body (if any): " + payload;
