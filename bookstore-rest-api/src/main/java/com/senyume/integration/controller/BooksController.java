4a1ba7b245b72989be4aaa82d58c06478cec8f87
Jigish Patel <jigish.patel@gmail.com>
2015-03-16 12:29:44 -0700

    added an "id" parameter for tracking

--- bookstore-rest-api/src/main/java/com/senyume/integration/controller/BooksController.java
+++ bookstore-rest-api/src/main/java/com/senyume/integration/controller/BooksController.java
@@ -3,10 +3,7 @@ package com.senyume.integration.controller;
 import com.senyume.integration.domain.Book;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,7 +18,8 @@ public class BooksController {
 
 	@RequestMapping(value = "/books", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.OK)
-	public List<Book> read(){
+	public ResponseMessage read(@RequestHeader(value = "api-tracking-id", required = false) String id){
+		
         List<Book> bookList = new ArrayList<Book>();
         Book book1 = new Book();
         book1.setName("The Jungle Book");
@@ -37,8 +35,35 @@ public class BooksController {
         book3.setName("Harry Potter and the Philosopher's Stone");
         book3.setAuthor("J. K. Rowling");
         bookList.add(book3);
-
-        return bookList;
+		
+        return new ResponseMessage(id,bookList);
+		
 	}
 
+	private class ResponseMessage {
+		String id;
+		List<Book> bookList;
+		
+		ResponseMessage(String id, List<Book> bookList){
+			this.id = id;
+			this.bookList = bookList;
+			
+		}
+
+		public String getId() {
+			return id;
+		}
+
+		public void setId(String id) {
+			this.id = id;
+		}
+
+		public List<Book> getBookList() {
+			return bookList;
+		}
+
+		public void setBookList(List<Book> bookList) {
+			this.bookList = bookList;
+		}
+	}
 }
